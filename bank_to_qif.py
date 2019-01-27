161891a4172d17e356756f249ed56ce95d05bd80
Daniel Engvall <daniel@engvalls.eu>
2019-01-27 11:24:07 +0100

    Renaming project and rough refactoring to fit pep8

--- bank_to_qif.py
+++ bank_to_qif.py
@@ -3,23 +3,23 @@
 
 __version__ = "$Revision: 20150201.92"
 
-dictCatBankDesc = {'Expenses:Car:Parking': ['Solna Stad'],
+dict_cat_bank_desc = {'Expenses:Car:Parking': ['Solna Stad'],
                    'Expenses:Work:Unemployment Fund': ['.*UNIONEN.*'],
                    'Expenses:Home:Services': ['.*Netflix.*'],
                    'Expenses:Home:Home Insurance': ['.*Hemf.rs.kring.*'],
                    'Expenses:Home:Mortgage Loan': ['.*VERFRING \d{11}.*', 'L.neavi\s\d+'],
                    'Expenses:Home:Computer Services': ['.*spideroak.*', '.*Evernote.*', '.*City Network.*', '.*iPeer.*', '.*JibJab.*'],
                    'Expenses:Health:Gym': ['.*Fitness 24.*'],
-                   'Expenses:Health:Eye': ['.*Synsam.*'],
-                   'Expenses:Car:Insurance': ['.*Falck.*', '.*Bilf.rs.kring.*'],
-                   'Assets:Current Assets:Savings Account': ['.*besparing.*', '.*spara.*', '.*verf. 9159.*', '.*R.NTEKONTO*'],
-                   'Expenses:Entertainment:Magazines': ['.*Datormagazin.*'],
-                   'Expenses:Credit Card': ['.*eurocard.*'],
-                   'Income:Salary': ['^L.n\b', '.*salary.*', '.*\(\)\d{9}.*', 'LN.*', '.*T\sL.N$'],
-                   'Expenses:Uncategorized': ['.*Paypal.*'],
-                   'Expenses:Boat:Fees': ['.*Marinpool.*']}
+                      'Expenses:Health:Eye': ['.*Synsam.*'],
+                      'Expenses:Car:Insurance': ['.*Falck.*', '.*Bilf.rs.kring.*'],
+                      'Assets:Current Assets:Savings Account': ['.*besparing.*', '.*spara.*', '.*verf. 9159.*', '.*R.NTEKONTO*'],
+                      'Expenses:Entertainment:Magazines': ['.*Datormagazin.*'],
+                      'Expenses:Credit Card': ['.*eurocard.*'],
+                      'Income:Salary': ['^L.n\b', '.*salary.*', '.*\(\)\d{9}.*', 'LN.*', '.*T\sL.N$'],
+                      'Expenses:Uncategorized': ['.*Paypal.*'],
+                      'Expenses:Boat:Fees': ['.*Marinpool.*']}
 
-dictCatBankCat = {'Assets:Current Assets:Retirement Savings': ['Pensionsparande'],
+dir_cat_bank_cat = {'Assets:Current Assets:Retirement Savings': ['Pensionsparande'],
                   'Expenses:Car:Fees': ['Fordonsskatt', '.vrigt: Bil och transport', 'V.gtull'],
                   'Expenses:Car:Gas': ['Br.nsle'],
                   'Expenses:Car:Parking': ['Parkering'],
@@ -27,28 +27,28 @@ dictCatBankCat = {'Assets:Current Assets:Retirement Savings': ['Pensionsparande'
                   'Expenses:Child': ['Barnartiklar'],
                   'Expenses:Clothes': ['Kl.der och skor'],
                   'Expenses:Computer': ['Dator och elektronik'],
-                  'Expenses:Dining': ['Restaurang och kaf.*', 'Snabbmat'],
-                  'Expenses:Entertainment:Alcohol': ['Alkohol'],
-                  'Expenses:Entertainment:Travel': ['.vrigt: Semester och resor'],
-                  'Expenses:Groceries': ['Livsmedel'],
-                  'Expenses:Health:Gym': ['Friskv.rd och tr.ningskort'],
-                  'Expenses:Home:Electric': ['El och v.rme'],
-                  'Expenses:Home:Rent': ['Hyra', 'Boendeavgifter'],
-                  'Expenses:Home:Services': ['Tv, telefoni och internet'],
-                  'Expenses:Work:Income Insurance': ['Inkomstf.rs.kring'],
-                  'Expenses:Interest:Mortgage Interest': ['Bol.n'],
-                  'Expenses:Helth:Eye': ['Syn och h.rsel'],
-                  'Expenses:Health:Misc': ['L.kare, sjukv.rd, tandl.kare', 'Medicin'],
-                  'Expenses:Public Transportation': ['Flyg, hyrbil och semestertransport', 'Taxi'],
-                  'Expenses:Uncategorized': ['.vrigt: Okategoriserade utgifter', 'Uttagsautomat', 'Hotel och .vernattning', '.vrigt: Boende och hush.ll', '.vrigt: Shopping och service', 'M.bler och interi.r', 'Renovering och underh.ll', 'Bio, teater, konserter etc', 'Hobby', 'Nattklubb, dansst.lle, bar', 'Skidor och vintersport', 'Liv- och sjukf.rs.kring', 'G.vor', 'B.cker och spel', 'Smycken', 'Tr.dg.rd', 'K.p av konst', 'N.jen under semester', 'Prenumerationer och tidningar', '.vrigt: Restauranger och n.jen', 'Film, DVD etc', 'Tobak, snus, cigaretter etc', 'Leksaker', 'Kollektivtrafik', 'Bankavgifter', 'Bageri', '.vrigt: Fritid', 'Kiosker, glassbarer etc', 'Musik och instrument', 'Skol- och fritidshemavgifter', '.vrigt: Mat', 'Hemf.rs.kring', 'Sk.nhetsprodukter', 'Kosttillskott och vitaminer', 'Kurslitteratur och kontorsvaror', 'F.reningsliv', '.verf.ring mellan egna konton'],
-                  'Expenses:Work:Unemployment Fund': ['A-Kassa'],
-                  'Income:Other Income': ['.vrigt: Utl.gg och .terbetalda utl.gg', 'Okategoriserad inkomst'],
-                  'Income:Tax Refund': ['Skatte.terb.ring'],
-                  'Expenses:Credit Card': ['Avbetalning konsumtionsl.n']}
+                    'Expenses:Dining': ['Restaurang och kaf.*', 'Snabbmat'],
+                    'Expenses:Entertainment:Alcohol': ['Alkohol'],
+                    'Expenses:Entertainment:Travel': ['.vrigt: Semester och resor'],
+                    'Expenses:Groceries': ['Livsmedel'],
+                    'Expenses:Health:Gym': ['Friskv.rd och tr.ningskort'],
+                    'Expenses:Home:Electric': ['El och v.rme'],
+                    'Expenses:Home:Rent': ['Hyra', 'Boendeavgifter'],
+                    'Expenses:Home:Services': ['Tv, telefoni och internet'],
+                    'Expenses:Work:Income Insurance': ['Inkomstf.rs.kring'],
+                    'Expenses:Interest:Mortgage Interest': ['Bol.n'],
+                    'Expenses:Helth:Eye': ['Syn och h.rsel'],
+                    'Expenses:Health:Misc': ['L.kare, sjukv.rd, tandl.kare', 'Medicin'],
+                    'Expenses:Public Transportation': ['Flyg, hyrbil och semestertransport', 'Taxi'],
+                    'Expenses:Uncategorized': ['.vrigt: Okategoriserade utgifter', 'Uttagsautomat', 'Hotel och .vernattning', '.vrigt: Boende och hush.ll', '.vrigt: Shopping och service', 'M.bler och interi.r', 'Renovering och underh.ll', 'Bio, teater, konserter etc', 'Hobby', 'Nattklubb, dansst.lle, bar', 'Skidor och vintersport', 'Liv- och sjukf.rs.kring', 'G.vor', 'B.cker och spel', 'Smycken', 'Tr.dg.rd', 'K.p av konst', 'N.jen under semester', 'Prenumerationer och tidningar', '.vrigt: Restauranger och n.jen', 'Film, DVD etc', 'Tobak, snus, cigaretter etc', 'Leksaker', 'Kollektivtrafik', 'Bankavgifter', 'Bageri', '.vrigt: Fritid', 'Kiosker, glassbarer etc', 'Musik och instrument', 'Skol- och fritidshemavgifter', '.vrigt: Mat', 'Hemf.rs.kring', 'Sk.nhetsprodukter', 'Kosttillskott och vitaminer', 'Kurslitteratur och kontorsvaror', 'F.reningsliv', '.verf.ring mellan egna konton'],
+                    'Expenses:Work:Unemployment Fund': ['A-Kassa'],
+                    'Income:Other Income': ['.vrigt: Utl.gg och .terbetalda utl.gg', 'Okategoriserad inkomst'],
+                    'Income:Tax Refund': ['Skatte.terb.ring'],
+                    'Expenses:Credit Card': ['Avbetalning konsumtionsl.n']}
 
 
-def cleanSkandiaExcelXML(filename, output):
-    ''' Clear out the <x: etc from the file '''
+def clean_skandia_excel_xml(filename, output):
+    """ Clear out the <x: etc from the file """
     import re
 
     f = open(filename, 'r')
@@ -65,10 +65,10 @@ def cleanSkandiaExcelXML(filename, output):
     w.close()
 
 
-def cleanList(listObject):
+def clean_list(list_object):
     ''' Remove header, get all columns and replace properly and create list like ...  [[u'TAXI J\xd6NK\xd6PING', u'Taxi', u'2010-10-12', u'-278.0000']]'''
     import re
-    rows = listObject[0]
+    rows = list_object[0]
     rows.pop(0)
     result = []
 
@@ -86,7 +86,7 @@ def cleanList(listObject):
     return result
 
 
-def parseExcelXML(filename):
+def parse_excel_xml(filename):
     ''' return a list of rows '''
     from xml.sax import parse
     from xml.sax import handler
@@ -117,35 +117,35 @@ def parseExcelXML(filename):
             elif name == "Table":
                 self.tables.append(self.rows)
 
-    excelHandler = ExcelHandler()
-    parse(filename, excelHandler)
-    return excelHandler.tables
+    excel_handler = ExcelHandler()
+    parse(filename, excel_handler)
+    return excel_handler.tables
 
 
-def checkInExisting(existingFile, date, amount):
+def checj_in_existing(existing_file, date, amount):
     ''' Check if date and amount exists in file '''
     import re
-    existingFile.seek(0)
+    existing_file.seek(0)
 
-    if existingFile:
-        for line in existingFile.readlines():
+    if existing_file:
+        for line in existing_file.readlines():
             try:
-                dateFile, dateAmount = re.match(r'([^;]+);[^;]+;[^;]+;([\d\-]+)$', line).groups()
+                date_file, date_amount = re.match(r'([^;]+);[^;]+;[^;]+;([\d\-]+)$', line).groups()
             except:
-                print "Wrong line format in %s: %s" % (existingFile.name, line)
+                print "Wrong line format in %s: %s" % (existing_file.name, line)
             else:
-                if str(date) == str(dateFile) and str(amount) == str(dateAmount):
+                if str(date) == str(date_file) and str(amount) == str(date_amount):
                     return True
     # If none found
     return False
 
 
-def addToExisting(existingFile, date, description, bankCategory, amount):
+def add_to_existing(existing_file, date, description, bank_category, amount):
     ''' Adding a record to the existing file '''
-    existingFile.write("%s;%s;%s;%s\n" % (date, description.encode('ascii', 'ignore'), bankCategory, amount))
+    existing_file.write("%s;%s;%s;%s\n" % (date, description.encode('ascii', 'ignore'), bank_category, amount))
 
 
-def convertListByCat(bankList, dictCatBankDec, dictCatBankCat, **args):
+def convert_list_by_cat(bank_list, dict_cat_bank_dec, dict_cat_bank_cat, **args):
     ''' Converting Categories based on dictionaries '''
     import re
     from datetime import datetime
@@ -156,14 +156,14 @@ def convertListByCat(bankList, dictCatBankDec, dictCatBankCat, **args):
     result = []
 
     # Iterate through all transactions
-    for transaction in bankList:
+    for transaction in bank_list:
         if len(transaction) == 5:
-            description, bankCategory, date, amount, other = transaction
+            description, bank_category, date, amount, other = transaction
         elif len(transaction) == 4:
-            description, bankCategory, date, amount = transaction
+            description, bank_category, date, amount = transaction
         elif len(transaction) == 3:
             description, date, amount = transaction
-            bankCategory = uncat
+            bank_category = uncat
         else:
             print "Wrong number of columns: %s" % (str(transaction))
 
@@ -178,36 +178,36 @@ def convertListByCat(bankList, dictCatBankDec, dictCatBankCat, **args):
         cat_found = False
 
         # Second check for matching of description
-        for category, expressions in dictCatBankDec.items():
+        for category, expressions in dict_cat_bank_dec.items():
             for expression in expressions:
                 # Check if expression match with description
                 if re.match(expression, description, flags=re.IGNORECASE) and not cat_found:
                     # Found match replace/assign category
-                    bankCategory = category
+                    bank_category = category
                     cat_found = True
                     if verbose:
-                        print "Found match in description for %s - updated record |%s|%s|%s|%s|" % (expression, description.encode('ascii', 'ignore'), bankCategory, date, amount)
+                        print "Found match in description for %s - updated record |%s|%s|%s|%s|" % (expression, description.encode('ascii', 'ignore'), bank_category, date, amount)
 
         # First check if matching of category is found
-        for category, expressions in dictCatBankCat.items():
+        for category, expressions in dict_cat_bank_cat.items():
             for expression in expressions:
                 # Check if expression match with description
-                if re.match(expression, bankCategory, flags=re.IGNORECASE) and not cat_found:
+                if re.match(expression, bank_category, flags=re.IGNORECASE) and not cat_found:
                     # Found match replace/assign category
-                    bankCategory = category
+                    bank_category = category
                     cat_found = True
                     if verbose:
-                        print "Found match of Category for %s - updated record |%s|%s|%s|%s|" % (expression, description.encode('ascii', 'ignore'), bankCategory, date, amount)
+                        print "Found match of Category for %s - updated record |%s|%s|%s|%s|" % (expression, description.encode('ascii', 'ignore'), bank_category, date, amount)
 
         # If not catefories found
         if not cat_found:
-                bankCategory = uncat
+                bank_category = uncat
 
-        result.append((date, description, bankCategory, amount))
+        result.append((date, description, bank_category, amount))
     return result
 
 
-def createQifHeader(output, account):
+def create_qif_header(output, account):
     ''' Function to create QIF Header '''
     output.write('!Account\n')
     output.write("N%s\n" % args.account)
@@ -215,7 +215,7 @@ def createQifHeader(output, account):
     output.write('^\n')
 
 
-def addQifRecord(output, date, description, category, amount):
+def add_qif_record(output, date, description, category, amount):
     ''' Function to create QIF file '''
     description = description.encode('ascii', 'ignore')
     output.write('!Type:Bank\n')
@@ -239,10 +239,10 @@ if __name__ == "__main__":
     parser.add_argument('--verbose', help="Verbose mode", action='store_true')
     args = parser.parse_args()
 
-    cleanSkandiaExcelXML(args.input.name, TEMP)
-    skandiaList = parseExcelXML(TEMP)
-    skandiaList = cleanList(skandiaList)
-    skandiaList = convertListByCat(skandiaList, dictCatBankDesc, dictCatBankCat, existing=args.existing, verbose=args.verbose)
+    clean_skandia_excel_xml(args.input.name, TEMP)
+    skandiaList = parse_excel_xml(TEMP)
+    skandiaList = clean_list(skandiaList)
+    skandiaList = convert_list_by_cat(skandiaList, dict_cat_bank_desc, dir_cat_bank_cat, existing=args.existing, verbose=args.verbose)
 
     row_no = 0
     existing_no = 0
@@ -250,21 +250,21 @@ if __name__ == "__main__":
 
     if args.input and args.output:
         # Adding header to output
-        createQifHeader(args.output, args.account)
+        create_qif_header(args.output, args.account)
 
         # Parse through all records in input
         for row in skandiaList:
             row_no += 1
             date, description, category, amount = row
             # Check if row exist in existingfile and if not true
-            if not checkInExisting(args.existing, date, amount):
+            if not checj_in_existing(args.existing, date, amount):
                 if args.verbose:
                     print "Adding record to existing: %s, %s, %s, %s" % (date, description.encode('ascii', 'ignore'), amount, category)
                 existing_no += 1
                 # Adding record to existing
-                addToExisting(args.existing, date, description, category, amount)
+                add_to_existing(args.existing, date, description, category, amount)
                 # Add record to QIF output
-                addQifRecord(args.output, date, description, category, amount)
+                add_qif_record(args.output, date, description, category, amount)
             else:
                 if args.verbose:
                     print "Already or duplicate record in %s: %s" % (args.existing.name, row)
@@ -278,4 +278,3 @@ if __name__ == "__main__":
         args.output.close()
     if args.input:
         args.input.close()
-    # print skandiaList
